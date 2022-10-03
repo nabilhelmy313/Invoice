2a03a13521a991ee6f17f5869a7b2af6eb3f498e
Nabil Helmy <nabiilhelmy313@gmail.com>
2022-10-03 16:02:39 +0200

    NABIL

---
+++ Persistence/Repositories/InvoiceHDRRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.Repositories;
+using Bases.Application;
+using Bases.Infrastructure;
+using Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class InvoiceHDRRepository:BaseRepostiory<InvoiceHDR,InvoiceDbContext,int>,IInvoiceHDRRepository
+    {
+        public InvoiceHDRRepository(InvoiceDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
